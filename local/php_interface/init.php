65b29db3c3f31451bb8a94351bf11092bcb24e68
User A0794759 <a0794759@sol.from.sh>
2024-07-24 19:47:16 +0300

    set log file path

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,7 @@
 <?php
 
+define('LOG_FILENAME', $_SERVER['DOCUMENT_ROOT'].'/local/logs/log.txt');
+
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/functions.php')) {
     require $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/functions.php';
 }
